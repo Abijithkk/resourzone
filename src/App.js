3b122269b559b42c3214152a9735beb99dd5442d
codeedexbackend <prithew.codeedex@gmail.com>
2024-11-19 15:46:29 +0530

    added commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './Pages/Home';
+import Hrm from './Pages/Hrm';
+import Career from './Pages/Career';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Footerbottom from './components/Footerbottom';
+import Enquiry from './Pages/Enquiry';
+import Application from './Pages/Application';
+import Privacy from './Pages/Privacy';
+import Terms from './Pages/Terms';
+import { Commet } from 'react-loading-indicators';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const location = useLocation();
+  const isNoHeaderFooterPage = location.pathname === '/' || location.pathname === '/enquiry' || location.pathname === '/application';
+  const [loading, setLoading] = useState(location.pathname !== '/');
+
+  useEffect(() => {
+    if (location.pathname !== '/') {
+      const timer = setTimeout(() => setLoading(false), 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [location.pathname]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (
+        <div className="loader-container">
+          <Commet color="#22CC9D" size="medium" text="" textColor="" />
+        </div>
+      ) : (
+        <>
+          {!isNoHeaderFooterPage && <Header />}
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/hrm" element={<Hrm />} />
+              <Route path="/career" element={<Career />} />
+              <Route path="/enquiry" element={<Enquiry />} />
+              <Route path="/application" element={<Application />} />
+              <Route path="/privacy" element={<Privacy />} />
+              <Route path="/terms" element={<Terms />} />
+            </Routes>
+          </div>
+          {!isNoHeaderFooterPage && <Footer />}
+          {!isNoHeaderFooterPage && <Footerbottom />}
+        </>
+      )}
     </div>
   );
 }
